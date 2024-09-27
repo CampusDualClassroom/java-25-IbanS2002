314fb8b9c9a68f557ec82a60fc5ddabb908b25b8
Ivan Silvosa Rodriguez <ivansrlsy@gmail.com>
2024-09-27 12:20:09 +0200

    Ejercicio Resuelto

--- src/main/java/com/campusdual/classroom/Exercise25.java
+++ src/main/java/com/campusdual/classroom/Exercise25.java
@@ -4,15 +4,26 @@ import java.util.Stack;
 
 public class Exercise25 {
     public static Stack<String> createStack() {
-
+        Stack<String>pila = new Stack<String>();
+        pila.push("Smith");
+        pila.push("Montessori");
+        pila.push("Peralta");
+        pila.push("House");
+        return pila;
     }
 
     public static void printAndEmptyStack(Stack<String> stack) {
 
+        while (!stack.isEmpty()) {
+            // Imprimimos el elemento en la cima y lo quitamos de la pila
+            System.out.println(stack.pop());
+        }
     }
 
     public static void main(String[] args) {
+        Stack<String>pilaR=createStack();
 
+        printAndEmptyStack(pilaR);
     }
 
 
